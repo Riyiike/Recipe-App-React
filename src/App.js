42571065ba0e27664c4b0d3cc9771c16ba1b0d8a
Riyiike <odusanyaiyanuoluwa@gmail.com>
2020-10-06 00:02:19 -0800

    adding api key and id

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const APP_ID = "67259000";
+  const APP_KEY = "e73763935af70ea5fcfaa91c7675d76b";
+
   return <div className="App"></div>;
-}
+};
 
 export default App;
