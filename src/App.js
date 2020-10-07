5ec3c88a82a50ace4585d188557f1936d5bf062a
Riyiike <odusanyaiyanuoluwa@gmail.com>
2020-10-06 23:59:07 -0800

    api request

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { UseEffect, UseState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
   const APP_ID = "67259000";
   const APP_KEY = "e73763935af70ea5fcfaa91c7675d76b";
+  // const Req = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
-  return <div className="App"></div>;
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    console.log(data);
+  };
+  return (
+    <div className="App">
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button classNmae="search-button" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default App;
